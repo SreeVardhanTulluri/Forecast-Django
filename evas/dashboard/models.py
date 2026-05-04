819417eda85a13c80e8a3fbc1a7ae81af882ce7a
SreeVardhanTulluri <TULLURISREEVARDHAN@GMAIL.COM>
2026-05-04 15:46:22 +0530

    Added all related Files

---
+++ evas/dashboard/models.py
@@ -0,0 +1,52 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Inventory(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+
+    clientno = models.IntegerField(db_column='CLIENTNO')
+    gloryid = models.CharField(db_column='GLORYID', max_length=30)
+    startcash = models.IntegerField(db_column='STARTCASH')
+    usamt = models.DecimalField(db_column='USAMT', max_digits=14, decimal_places=0)
+    cashamt = models.DecimalField(db_column='CASHAMT', max_digits=14, decimal_places=0)
+    siteno = models.IntegerField(db_column='SITENO', null=True, blank=True)
+    lasttrace = models.CharField(db_column='LASTTRACE', max_length=20, null=True, blank=True)
+    traceversion = models.IntegerField(db_column='TRACEVERSION')
+
+    class Meta:
+        managed = False
+        db_table = '[EVASAPP].[INVENTORY_V]'
+
+
+class InventoryDtl(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+
+    clientno = models.IntegerField(db_column='CLIENTNO')
+    gloryid = models.CharField(db_column='GLORYID', max_length=30)
+    startcash = models.IntegerField(db_column='STARTCASH')
+    looseflg = models.IntegerField(db_column='LOOSEFLG')
+    mediano = models.IntegerField(db_column='MEDIANO')
+    amount = models.DecimalField(db_column='AMOUNT', max_digits=18, decimal_places=0)
+    siteno = models.IntegerField(db_column='SITENO', null=True, blank=True)
+    traceversion = models.IntegerField(db_column='TRACEVERSION')
+    lasttrace = models.CharField(db_column='LASTTRACE', max_length=20, null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = '[EVASAPP].[INVENTORYDTL_V]'
+
+
+class Mediamap(models.Model):
+    mediano = models.IntegerField(db_column='MEDIANO', primary_key=True)  # Field name made lowercase.
+    value = models.DecimalField(db_column='VALUE', max_digits=12, decimal_places=0, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = '[EVASAPP].[MEDIAMAP]'
